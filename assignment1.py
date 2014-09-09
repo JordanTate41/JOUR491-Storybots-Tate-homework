3b620af4da81cdcb908c56f2143bccccbe42a2c7
Jordan Tate <jordantate41@yahoo.com>
2014-09-09 15:37:25 -0500

    Adding the Madlibs assignments that completes sentences in python.

---
+++ assignment1.py
@@ -0,0 +1,5 @@
+plural_noun = "chickens"
+verb = "squawk"
+
+
+print "As a Jedi master, I've taught some of the most talented %s in the galaxy how to %s with a lightsaber." % (plural_noun, verb)
---
+++ assignment2.py
@@ -0,0 +1,4 @@
+variables = [("Jordan", "fought"), ("Ben", "cried"), ("Trent", "pooped his pants"), ("Alan", "ate glue"), ("tim", "bullied a kid"), ("matt", "pouted"), ("evan", "was late"), ("josh", "peed his pants"), ("alex", "got an f"), ("brent", "threw up")]
+
+for item in variables:
+    print ("when %s was naughty, he %s today" % item)
